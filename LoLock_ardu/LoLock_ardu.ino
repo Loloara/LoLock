9e7b4f64960819e733e922d4d1ba894a56a4c52e
Loloara <addy94@naver.com>
2017-07-21 15:28:52 +0900

    second stabilize code from jinwon's

--- LoLock_ardu/LoLock_ardu.ino
+++ LoLock_ardu/LoLock_ardu.ino
@@ -1,73 +1,99 @@
-#include <SoftwareSerial.h>
 #include <LoRaShield.h>
 #include <Servo.h>
+#include <SoftwareSerial.h>
 
-LoRaShield LoRa1(10, 11);
-SoftwareSerial BTSerial(0,1); // (TX, RX)
+#define BlueRxD 3           //Bluetooth RX
+#define BlueTxD 2           //Bluetooth TX
+#define SERVOPIN 5          //servo Motor Pin
+#define TriggerPin 8        //Trig Pin
+#define EchoPin 9           //Echo Pin
+#define Tx 10               //LoRa
+#define Rx 11               //LoRa
+#define PUSH 12             //pushButton Pin
+#define OPEN 60          //angle of door open
+#define CLOSE 0          //angle of door close
+
+LoRaShield LoRa(Tx, Rx);
+SoftwareSerial BTSerial(BlueRxD,BlueTxD); //(RX, TX)
 Servo servo;
 
-int pushButton = A5; // push Button pin
-const int servoPin = 5; // 서보모터 pin
-const int TriggerPin = 8; //Trig pin
-const int EchoPin = 9; //Echo pin
-long Duration = 0;
-int cnt = 0;
-boolean doorVal = false;
-int led = 13;
-int angle = 0; // servo potision in degrees
+String s,m;              //LoRa.ReadLine(), GetMessage()
+byte data;
+long duration_val = 0;
+boolean push_cnt = false;  //서보모터가 돌아가 있는지 여부
+int cnt = 0;               //초음파 센서 알고리즘
+boolean door_val = false; //문의 상태 TRUE:OPEN FALSE:CLOSE
+boolean asButton = false; //버튼을 통해 문이 열렸는지 확인하는 변수
 
 void setup() {
-  pinMode(TriggerPin, OUTPUT); // Trigger is an output pin
-  pinMode(EchoPin, INPUT); // Echo is an input pin
-  Serial.begin(9600); // Serial Output
-  BTSerial.begin(9600); // Bluetooth Output
-  LoRa1.begin(38400);
-  pinMode(led, OUTPUT);
-  servo.attach(servoPin);
-  servo.write(0);
+  BTSerial.begin(115200);
+  Serial.begin(115200);
+  LoRa.begin(38400);
+  pinMode(PUSH, INPUT);
+  servo.attach(SERVOPIN);
+  servo.write(CLOSE);
+  initializeBoardSetting();
 }
 
 void loop() {
 
-  int i = analogRead(5); // A5 아날로그 입력으로부터 전압을 읽어옴
-  if(i > 1000) {  //전압값이 1000보다 크면 (약 4.88V)
-    servo.write(60);  //모터 60도 회전
-    digitalWrite(led, HIGH); // 내장 LED ON
-  }
-  else if(i < 1000) {
-    servo.write(0);
-    digitalWrite(led, LOW);
-  }
-  while (LoRa1.available())
-  {
-    String s = LoRa1.ReadLine();
-    Serial.print("LoRa.ReadLine() = ");
-    Serial.println(s);
- 
-    String m = LoRa1.GetMessage();
-    Serial.print("LoRa.GetMessage() = ");
-    Serial.println(m);
-
-    if(m == "260100")
-      digitalWrite(led, HIGH);
-    else if(m == "260101")
-      digitalWrite(led, LOW);
-  }
-
-
+  pushButton();
+  DoorOpenState();
   
+  while (LoRa.available())
+  {
+    s = LoRa.ReadLine();
+    //Serial.print("LoRa.ReadLine() = ");
+    //Serial.println(s);
+
+    m = LoRa.GetMessage();
+    if(m != ""){
+    Serial.println("Recv from LoRa : " + m);
+      if(m == "22"){    //command that open the door by LoRa
+        openDoorByLoRa();
+      }
+    }
+  }
+}
+
+void initializeBoardSetting(){
+  
+}
+
+void pushButton()
+{
+  int i = digitalRead(PUSH);  // A5 아날로그 입력으로 전압을 읽어들임
+  delay(100);
+  if(!i){     // i 상태 == 1
+    servo.write(OPEN);  //모터 60도 회전
+    asButton = true;
+    push_cnt = true;
+  }
+  else {
+    servo.write(CLOSE);
+    if(push_cnt)
+    {
+      LoRa.SendMessage("Going Out", HEX);
+      Serial.println("Button Clicked");
+      push_cnt = false;
+    }
+  }
+}
+
+void DoorOpenState()
+{
   digitalWrite(TriggerPin, LOW);
   delayMicroseconds(2);
   digitalWrite(TriggerPin, HIGH); // Trigger pin to HIGH
   delayMicroseconds(10); // 10us high
   digitalWrite(TriggerPin, LOW); // Trigger pin to HIGH
+ 
+  //duration_val = pulseIn(EchoPin, HIGH); // Waits for the echo pin to get high
+  //returns the Duration in microseconds
 
-  Duration = pulseIn(EchoPin, HIGH); // Waits for the echo pin to get high
-  // returns the Duration in microseconds
-
-  long Distance_mm = Distance(Duration); // Use function to calculate the distance
+  long Distance_mm = Distance(duration_val); // Use function to calculate the distance
   
-  if(Distance_mm > 500 && Distance_mm < 30000) 
+  /*if(Distance_mm > 500 && Distance_mm < 30000) 
   {
     cnt++;
     delay(100);
@@ -78,40 +104,40 @@ void loop() {
     delay(100);
   }
   if(cnt > 10)
-  {
-    doorVal = true;
+  {  
+    door_val = true;
     cnt = 11;
   }
   else if (cnt == 0)
-  {
-    doorVal = false;
-  }
+    door_val = false;
   if (cnt < 0)
-  {
-    cnt = 0;
-  }
-  /*if(doorVal)
-    BTSerial.begin(9600);
-  else
-    BTSerial.end();
-  Serial.print("Distance = "); // Output to serial
-  Serial.print(Distance_mm);
-  Serial.println(" mm");
-  if(doorVal)
-    Serial.println("Door Open!");
-  else
-    Serial.println("Door Closed");
-  */
-  delay(100); // Wait to do next measurement
+    cnt = 0; */
+  
+  
 }
-
-long Distance(long time)
+long Distance(long _time)
 {
   // Calculates the Distance in mm
   // ((time)*(Speed of sound))/ toward and backward of object) * 10
 
   long DistanceCalc; // Calculation variable
-  DistanceCalc = ((time / 2.9) / 2); // Actual calculation in mm
+  DistanceCalc = ((_time / 2.9) / 2); // Actual calculation in mm
   //DistanceCalc = time / 74 / 2; // Actual calculation in inches
   return DistanceCalc; // return calculated value
+}
+
+void openDoorByLoRa(void){
+  //if(Closed){
+  servo.write(OPEN);
+  delay(100);
+  servo.write(CLOSE);
+  asButton = true;
+  Serial.println("Open Success");
+  LoRa.SendMessage("Open Success",HEX);
+  return true;
+  //}
+  //if(Opened){
+  //Serial.println("Open Fail");
+  //LoRa.SendMessage("Open Fail",HEX);
+  // return false;}
 }
