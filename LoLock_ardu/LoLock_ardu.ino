801df530598dddbe7d25e937ab23ca5eab2ef7c6
Loloara <addy94@naver.com>
2017-07-22 21:08:06 +0900

    third stabilize code

--- LoLock_ardu/LoLock_ardu.ino
+++ LoLock_ardu/LoLock_ardu.ino
@@ -2,8 +2,8 @@
 #include <Servo.h>
 #include <SoftwareSerial.h>
 
-#define BlueRxD 3           //Bluetooth RX
 #define BlueTxD 2           //Bluetooth TX
+#define BlueRxD 3           //Bluetooth RX
 #define SERVOPIN 5          //servo Motor Pin
 #define TriggerPin 8        //Trig Pin
 #define EchoPin 9           //Echo Pin
@@ -14,7 +14,7 @@
 #define CLOSE 0          //angle of door close
 
 LoRaShield LoRa(Tx, Rx);
-SoftwareSerial BTSerial(BlueRxD,BlueTxD); //(RX, TX)
+SoftwareSerial BTSerial(BlueTxD, BlueRxD); //(RX, TX)
 Servo servo;
 
 String s,m;              //LoRa.ReadLine(), GetMessage()
@@ -26,19 +26,27 @@ boolean door_val = false; //문의 상태 TRUE:OPEN FALSE:CLOSE
 boolean asButton = false; //버튼을 통해 문이 열렸는지 확인하는 변수
 
 void setup() {
-  BTSerial.begin(115200);
-  Serial.begin(115200);
+  BTSerial.begin(9600);
+  Serial.begin(9600);
   LoRa.begin(38400);
   pinMode(PUSH, INPUT);
   servo.attach(SERVOPIN);
   servo.write(CLOSE);
-  initializeBoardSetting();
 }
 
 void loop() {
 
   pushButton();
   DoorOpenState();
+  
+  if (BTSerial.available()){ 
+    data = BTSerial.read();
+    Serial.write(data);
+  }  
+  if (Serial.available()){
+    data = Serial.read();
+    BTSerial.write(data);
+  }
   
   while (LoRa.available())
   {
@@ -56,14 +64,10 @@ void loop() {
   }
 }
 
-void initializeBoardSetting(){
-  
-}
-
 void pushButton()
 {
   int i = digitalRead(PUSH);  // A5 아날로그 입력으로 전압을 읽어들임
-  delay(100);
+  
   if(!i){     // i 상태 == 1
     servo.write(OPEN);  //모터 60도 회전
     asButton = true;
@@ -96,20 +100,28 @@ void DoorOpenState()
   /*if(Distance_mm > 500 && Distance_mm < 30000) 
   {
     cnt++;
-    delay(100);
+    
   }
   else
   {
     cnt--;
-    delay(100);
+    
   }
   if(cnt > 10)
   {  
     door_val = true;
+    if(asButton){
+      LoRa.SendMessage("Door is open inside",HEX);
+    }else{
+      LoRa.SendMessage("Door is open outside",HEX);
+    }
+    
     cnt = 11;
   }
-  else if (cnt == 0)
+  else if (cnt == 0){
     door_val = false;
+    asButton = false;
+   }
   if (cnt < 0)
     cnt = 0; */
   
@@ -129,9 +141,8 @@ long Distance(long _time)
 void openDoorByLoRa(void){
   //if(Closed){
   servo.write(OPEN);
-  delay(100);
+  
   servo.write(CLOSE);
-  asButton = true;
   Serial.println("Open Success");
   LoRa.SendMessage("Open Success",HEX);
   return true;
