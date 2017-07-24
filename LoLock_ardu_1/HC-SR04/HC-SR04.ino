cd36b97728247a175db68cebb0a1801a53981142
HanJinwon <nowniz93@gmail.com>
2017-07-24 21:12:38 +0900

    UltraSonic Function Redefined "nizpulseIn()"

--- LoLock_ardu_1/HC-SR04/HC-SR04.ino
+++ LoLock_ardu_1/HC-SR04/HC-SR04.ino
@@ -1,28 +1,69 @@
-const int TriggerPin = 8; //Trig pin
-const int EchoPin = 9; //Echo pin
+#include <LoRaShield.h>
+#include <Servo.h>
+
+LoRaShield LoRa1(10, 11);
+Servo servo;
+
+const int TriggerPin = 5; //Trig pin
+const int EchoPin = 6; //Echo pin
+#define servoPin 4 // servo Motor Pin
+
 long Duration = 0;
 int cnt = 0;
 int ccnt = 0;
 boolean doorVal = false;
+boolean stateFlag = true;
+    unsigned long t_1, t_2, t_3;
 void setup() {
   pinMode(TriggerPin, OUTPUT); // Trigger is an output pin
   pinMode(EchoPin, INPUT); // Echo is an input pin
-  Serial.begin(9600); // Serial Output
+  Serial.begin(115200); // Serial Output
+  LoRa1.begin(38400);
+  servo.attach(servoPin);
+  servo.write(0);
 }
 
 void loop() {
-  digitalWrite(TriggerPin, LOW);
-  delayMicroseconds(2);
+  float distance;
+  unsigned long p_t, c_t;
+  /*digitalWrite(TriggerPin, LOW);
+  delayMicroseconds(2);*/
+  digitalWrite(EchoPin, LOW);
   digitalWrite(TriggerPin, HIGH); // Trigger pin to HIGH
   delayMicroseconds(10); // 10us high
   digitalWrite(TriggerPin, LOW); // Trigger pin to HIGH
-
-  Duration = pulseIn(EchoPin, HIGH); // Waits for the echo pin to get high
-  // returns the Duration in microseconds
-
-  long Distance_mm = Distance(Duration); // Use function to calculate the distance
   
-  if(Distance_mm > 500 && Distance_mm < 30000) 
+  Duration = NizPulseIn(EchoPin, HIGH, 37)/5.82;
+  
+  distance = ((float)(340*Duration)/10000)/2; // Use function to calculate the distance
+  //Serial.println(stateFlag);
+ 
+  Serial.print("Duration : ");
+  Serial.println(Duration);
+  //Serial.print("t_1 : ");
+  //Serial.println(t_1);
+  //Serial.print("t_2 : ");
+  //Serial.println(t_2);
+  //Serial.print("t_3 : ");
+  //Serial.println(t_3);
+  while (LoRa1.available())
+    {
+      String s = LoRa1.ReadLine();
+      //Serial.print("LoRa.ReadLine() = ");
+      //Serial.println(s);
+ 
+      String m = LoRa1.GetMessage();
+      Serial.print("LoRa.GetMessage() = ");
+      Serial.println(m);
+
+      if(m == "26") {
+        //digitalWrite(led, HIGH);
+        servo.write(60);
+        delay(100);
+        servo.write(0);
+      }
+    }
+  /*if(Distance_mm > 500 && Distance_mm < 30000) 
   {
     cnt++;
     delay(100);
@@ -45,26 +86,43 @@ void loop() {
   {
     cnt = 0;
   }
-  /*
-  Serial.print("Distance = "); // Output to serial
-  Serial.print(Distance_mm);
-  Serial.println(" mm");
-  Serial.println(cnt);*/
-  if(doorVal)
+  */
+  
+  /*Serial.print("Duration : ");
+  Serial.println(Duration);
+  Serial.print("Distance : ");
+  Serial.print(distance);
+  Serial.println("cm");
+  delay(50);*/
+  /*if(doorVal)
   Serial.println("Door Open!");
   else
   Serial.println("Door Closed");
   
-  delay(100); // Wait to do next measurement
+  delay(100); // Wait to do next measurement*/
 }
 
-long Distance(long time)
-{
-  // Calculates the Distance in mm
-  // ((time)*(Speed of sound))/ toward and backward of object) * 10
-
-  long DistanceCalc; // Calculation variable
-  DistanceCalc = ((time / 2.9) / 2); // Actual calculation in mm
-  //DistanceCalc = time / 74 / 2; // Actual calculation in inches
-  return DistanceCalc; // return calculated value
+unsigned long NizPulseIn(int pin, int value, int timeout) { // the following comments assume that we're passing HIGH as value. timeout is in milliseconds
+    unsigned long now = micros();
+    while(digitalRead(pin) == value) { // wait if pin is already HIGH when the function is called, but timeout if it never goes LOW
+        if (micros() - now > (timeout*500)) {
+            return 0;
+        }
+    }
+    //t_1 = micros()/100;
+    now = micros(); // could delete this line if you want only one timeout period from the start until the actual pulse width timing starts
+    while (digitalRead(pin) != value) { // pin is LOW, wait for it to go HIGH befor we start timing, but timeout if it never goes HIGH within the timeout period
+        if (micros() - now > (timeout*500)) { 
+            return 0;
+        }
+    }
+    //t_2 = micros()/100;
+    now = micros();
+    while (digitalRead(pin) == value) { // start timing the HIGH pulse width, but time out if over timeout milliseconds
+        if (micros() - now > (timeout*500)) {
+            return 0;
+        }
+    }
+    //t_3 = micros()/100;
+    return micros() - now;
 }
--- LoLock_ardu_1/LoRaTest_with_Arduino/LoRaTest_with_Arduino.ino
+++ LoLock_ardu_1/LoRaTest_with_Arduino/LoRaTest_with_Arduino.ino
@@ -10,66 +10,73 @@ LoRaShield LoRa1(10, 11);
 SoftwareSerial BTSerial(0,1); //(RX, TX)
 Servo servo;
 
-int led = 13;
-int pushButton = 12;  // Pushbutton Pin
-int servoPin = 5; // servo Motor Pin
-int TriggerPin = 8; // Trig Pin
-int EchoPin = 9; // Echo Pin
+#define pushButton 8  // Pushbutton Pin
+#define servoPin 4 // servo Motor Pin
+#define TriggerPin 5 // Trig Pin
+#define EchoPin 6 // Echo Pin
+#define PiezoPin A1 // Piezo vibrator Pin
+
 long Duration = 0;
+float distance = 0;
 boolean pushCnt = false; // 서보모터가 돌아가 있는지 여부
-int cnt = 0;
-boolean doorVal = false;
+int cnt = 0;  //  문열림 상태체크용 카운트
+int bcnt_1 = 0; // 진동감지 상태체크용 카운트
+int bcnt_2 = 0;
+boolean doorVal = false; // 문의 열림/닫힘상태
 int angle = 0; // servo potision in degrees
+
 void setup() {
   // put your setup code here, to run once:
   BTSerial.begin(9600);
   Serial.begin(115200);
   LoRa1.begin(38400);
   pinMode(pushButton, INPUT);
-  pinMode(led, OUTPUT);
+  //pinMode(led, OUTPUT);
   servo.attach(servoPin);
   servo.write(0);
 }
 
 void loop() {
 
-  PushButton();
+  Pushing();
   DoorOpenState();
+  Emergency();
   
   while (LoRa1.available())
     {
       String s = LoRa1.ReadLine();
-      Serial.print("LoRa.ReadLine() = ");
-      Serial.println(s);
+      //Serial.print("LoRa.ReadLine() = ");
+      //Serial.println(s);
  
       String m = LoRa1.GetMessage();
       Serial.print("LoRa.GetMessage() = ");
       Serial.println(m);
 
-      if(m == "260010") {
-        digitalWrite(led, HIGH);
+      if(m == "26") {
+        //digitalWrite(led, HIGH);
         servo.write(60);
         delay(100);
         servo.write(0);
       }
-      else if(m == "260011")
-        digitalWrite(led, LOW);
+      else if(m == "260011") {
+        //digitalWrite(led, LOW);
+    }
     }
 }
 
-void PushButton()
+void Pushing()
 {
-  int i = digitalRead(pushButton);  // A5 아날로그 입력으로 전압을 읽어들임
-  delay(100);
-  if(!i){     // i 상태 == 1
+  int i = digitalRead(pushButton);  // 디지털입력 8번핀
+  
+  if(!i){             // i 상태 == 1
     servo.write(60);  //모터 60도 회전
-    pushCnt = true;
+    pushCnt = true;   //눌린상태 체크
   }
   else {
     servo.write(0);
     if(pushCnt)
     {
-      LoRa1.SendMessage("Click", HEX);
+      //LoRa1.SendMessage("Click", HEX);  //눌렀다 뗐을 때 로라전송
       Serial.println("Click!");
       pushCnt = false;
     }
@@ -78,26 +85,24 @@ void PushButton()
 
 void DoorOpenState()
 {
-  digitalWrite(TriggerPin, LOW);
-  delayMicroseconds(2);
   digitalWrite(TriggerPin, HIGH); // Trigger pin to HIGH
   delayMicroseconds(10); // 10us high
   digitalWrite(TriggerPin, LOW); // Trigger pin to HIGH
  
-  //Duration = pulseIn(EchoPin, HIGH); // Waits for the echo pin to get high
+  // Duration = pulseIn(EchoPin, HIGH); // Waits for the echo pin to get high
   // returns the Duration in microseconds
 
-  long Distance_mm = Distance(Duration); // Use function to calculate the distance
+  distance = ((float)(340*Duration)/10000)/2; // Use function to calculate the distance
   
-  /*if(Distance_mm > 500 && Distance_mm < 30000) 
+  if(distance > 500 && distance < 30000) 
   {
     cnt++;
-    delay(100);
+    //delay(100);
   }
   else
   {
     cnt--;
-    delay(100);
+    //delay(100);
   }
   if(cnt > 10)
   {  
@@ -107,18 +112,29 @@ void DoorOpenState()
   else if (cnt == 0)
     doorVal = false;
   if (cnt < 0)
-    cnt = 0; */
+    cnt = 0; 
   
   
 }
-long Distance(long time)
-{
-  // Calculates the Distance in mm
-  // ((time)*(Speed of sound))/ toward and backward of object) * 10
 
-  long DistanceCalc; // Calculation variable
-  DistanceCalc = ((time / 2.9) / 2); // Actual calculation in mm
-  //DistanceCalc = time / 74 / 2; // Actual calculation in inches
-  return DistanceCalc; // return calculated value
+void Emergency()
+{
+  int val;
+  val = analogRead(PiezoPin);
+  if(val > 1000)
+      bcnt_1++;
+  else
+      bcnt_2++;
+
+  if(bcnt_2 == 150)
+  {
+      bcnt_1 = 0;
+      bcnt_2 = 0;
+  }
+  if(bcnt_1 > 300)
+  {
+      Serial.println("!!!");
+      bcnt_1 = 0;
+  }
 }
 
